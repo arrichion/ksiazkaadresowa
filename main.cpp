90903f722a618efb5f62754797e02fb32e7a7a30
Rafal Tkaczewski <rafal.tkaczewski.programista@gmail.com>
2018-07-08 16:54:34 +0200

    Add functions: edytujOsobe, usunOsobe, zapisDoPliku

--- main.cpp
+++ main.cpp
@@ -3,80 +3,116 @@
 #include <windows.h>
 #include <conio.h>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 struct Osoba {
-    string imie, nazwisko, adres;
-    int id;
+    string imie="", nazwisko="", adres="";
+    int id=0;
 };
 
-void odczytZPliku(Osoba* osoby, int &liczbaOsob) {
-    fstream plik;
-    string linia;
-    int nrLinii=1;
-
-    plik.open("ksiazka.txt", ios::in);
-
-    if(plik.good()) {
-        while (getline(plik, linia)) {
-            switch (nrLinii) {
-            case 1:
-                osoby[liczbaOsob].id=atoi(linia.c_str());
-                break;
-            case 2:
-                osoby[liczbaOsob].imie=linia;
-                break;
-            case 3:
-                osoby[liczbaOsob].nazwisko=linia;
-                break;
-            case 4:
-                osoby[liczbaOsob].adres=linia;
-                break;
-
-            }
-            if(nrLinii==4) {
-                nrLinii=0;
-                ++liczbaOsob;
-            }
-            ++nrLinii;
-        }
-    }
-    plik.close();
-}
-
-void wyswietlOsobe(Osoba* osoby, int nrOsoby) {
+void wyswietlOsobe(vector<Osoba> &osoby, int nrOsoby) {
     cout<<osoby[nrOsoby].imie<<" "<<osoby[nrOsoby].nazwisko<<endl;
     cout<<osoby[nrOsoby].adres<<endl;
     cout<<"ID: "<<osoby[nrOsoby].id<<endl<<endl;
 }
 
-void wprowadzDane(Osoba* osoby, int &liczbaOsob) {
+void odczytZPliku(vector<Osoba> &osoby, int &liczbaOsob) {
+    fstream plik;
+    string linia ,nrIDstr;
+    char znak;
+    int nrDanej=1;
+    Osoba odczytanaOsoba;
+
+    plik.open("ksiazka.txt", ios::in);
+
+    if(plik.good()) {
+        while (getline(plik, linia)) {
+            osoby.push_back(odczytanaOsoba);
+            znak=' ';
+            for(int i=0; znak!='|' || nrDanej!=1; ++i) {
+                znak=linia[i];
+                if(znak=='|')
+                    nrDanej+=1;
+                else {
+                    switch (nrDanej) {
+                    case 1:
+                        nrIDstr+=znak;
+                        if(linia[i+1]=='|'){
+                            osoby[liczbaOsob].id=atoi(nrIDstr.c_str());
+                            nrIDstr="";
+                        }
+                        break;
+                    case 2:
+                        osoby[liczbaOsob].imie+=znak;
+                        break;
+                    case 3:
+                        osoby[liczbaOsob].nazwisko+=znak;
+                        break;
+                    case 4:
+                        osoby[liczbaOsob].adres+=znak;
+                        break;
+                    }
+                }
+                if(nrDanej==5) {
+                    nrDanej=1;
+                    ++liczbaOsob;
+                }
+            }
+        }
+    }
+    plik.close();
+}
+
+void zapisDoPliku(vector<Osoba> &osoby, int liczbaOsob) {
     fstream plik;
 
+    plik.open("ksiazka.txt",ios::out);
+
+    for(int i=0; i<liczbaOsob; ++i){
+        plik<<osoby[i].id<<"|";
+        plik<<osoby[i].imie<<"|";
+        plik<<osoby[i].nazwisko<<"|";
+        plik<<osoby[i].adres<<"|"<<endl;
+    }
+
+    plik.close();
+}
+
+void wprowadzDane(vector<Osoba> &osoby, int &liczbaOsob) {
+    fstream plik;
+    Osoba dodawanaOsoba;
+
+    cin.clear();
     cin.sync();
 
     cout<<"Podaj imie: ";
-    getline(cin, osoby[liczbaOsob].imie);
+    getline(cin, dodawanaOsoba.imie);
     cout<<"Podaj nazwisko: ";
-    getline(cin, osoby[liczbaOsob].nazwisko);
+    getline(cin, dodawanaOsoba.nazwisko);
     cout<<"Podaj adres: ";
-    getline(cin, osoby[liczbaOsob].adres);
-    osoby[liczbaOsob].id=liczbaOsob+1;
+    getline(cin, dodawanaOsoba.adres);
+    if(liczbaOsob==0)
+        dodawanaOsoba.id=1;
+    else
+        dodawanaOsoba.id=osoby[liczbaOsob-1].id+1;
+
+    osoby.push_back(dodawanaOsoba);
 
     plik.open("ksiazka.txt",ios::out | ios::app);
 
-    plik<<osoby[liczbaOsob].id<<endl;
-    plik<<osoby[liczbaOsob].imie<<endl;
-    plik<<osoby[liczbaOsob].nazwisko<<endl;
-    plik<<osoby[liczbaOsob].adres<<endl;
+    plik<<osoby[liczbaOsob].id<<"|";
+    plik<<osoby[liczbaOsob].imie<<"|";
+    plik<<osoby[liczbaOsob].nazwisko<<"|";
+    plik<<osoby[liczbaOsob].adres<<"|"<<endl;
 
     plik.close();
 
     ++liczbaOsob;
 }
 
-void wyszukajPoImieniu(Osoba* osoby, int liczbaOsob) {
+void wyszukajPoImieniu(vector<Osoba> &osoby, int liczbaOsob) {
     string imie;
     bool znalezionoOsobeWKsiazce = false;
 
@@ -92,7 +128,7 @@ void wyszukajPoImieniu(Osoba* osoby, int liczbaOsob) {
         cout<<endl<<"Brak osob o tym imieniu.";
 }
 
-void wyszukajPoNazwisku(Osoba* osoby, int liczbaOsob) {
+void wyszukajPoNazwisku(vector<Osoba> &osoby, int liczbaOsob) {
     string nazwisko;
     bool znalezionoOsobeWKsiazce = false;
 
@@ -105,18 +141,99 @@ void wyszukajPoNazwisku(Osoba* osoby, int liczbaOsob) {
             znalezionoOsobeWKsiazce=true;
         }
     if(znalezionoOsobeWKsiazce==false)
-        cout<<endl<<"Brak osob o tym imieniu.";
+        cout<<endl<<"Brak osob o tym nazwisku.";
 }
 
-void wyswietlListeOsob(Osoba* osoby, int liczbaOsob) {
+void wyswietlListeOsob(vector<Osoba> &osoby, int liczbaOsob) {
+    if(liczbaOsob==0)
+        cout<<"W ksiazce nie ma aktualnie zadnych zapisanych osob.";
+    else
+        for(int i=0; i<liczbaOsob; ++i)
+            wyswietlOsobe(osoby, i);
+}
+
+void usunOsobe(vector<Osoba> &osoby, int &liczbaOsob) {
+    int nrID;
+    char wybor;
+    bool znalezionoOsobe = false;
+    vector <Osoba>::iterator itr;
+
+    cout<<"Podaj nr ID osoby, ktora chcesz usunac: ";
+    cin>>nrID;
+
     for(int i=0; i<liczbaOsob; ++i)
-        wyswietlOsobe(osoby, i);
+        if(osoby[i].id==nrID) {
+            znalezionoOsobe = true;
+            itr = osoby.begin()+i;
+            cout<<"Czy na pewno chcesz usunac "<<osoby[i].imie<<" "<<osoby[i].nazwisko<<"? (t/n)";
+            wybor=getch();
+            switch(wybor) {
+            case 't':
+                cout<<endl<<"Usunieto "<<osoby[i].imie<<" "<<osoby[i].nazwisko<<" z ksiazki.";
+                osoby.erase(itr);
+                --liczbaOsob;
+                osoby.resize(liczbaOsob);
+                zapisDoPliku(osoby, liczbaOsob);
+                break;
+            case 'n':
+                cout<<endl<<osoby[i].imie<<" "<<osoby[i].nazwisko<<" pozostal w ksiazce.";
+                break;
+            }
+        }
+    if(znalezionoOsobe==false)
+        cout<<"Nie ma w ksiazce osoby o podanym ID.";
+}
+
+void edytujOsobe (vector<Osoba> &osoby, int &liczbaOsob) {
+    int nrID;
+    char wybor;
+    string imie;
+    bool znalezionoOsobe = false;
+
+    cout<<"Podaj nr ID osoby, ktora chcesz edytowac: ";
+    cin>>nrID;
+
+    for(int i=0; i<liczbaOsob; ++i) {
+        if(osoby[i].id==nrID) {
+            znalezionoOsobe = true;
+            cout<<endl<<osoby[i].imie<<" "<<osoby[i].nazwisko<<endl;
+            cout<<osoby[i].adres<<endl;
+
+            cout<<endl;
+            cout<<"1. Edytuj imie."<<endl;
+            cout<<"2. Edytuj nazwisko."<<endl;
+            cout<<"3. Edytuj adres."<<endl;
+
+            wybor = getch();
+            cout<<endl;
+
+            cin.clear();
+            cin.sync();
+            switch(wybor) {
+            case '1':
+                cout<<"Podaj nowe imie: ";
+                getline(cin, osoby[i].imie);
+                break;
+            case '2':
+                cout<<"Podaj nowe nazwisko: ";
+                getline(cin, osoby[i].nazwisko);
+                break;
+            case '3':
+                cout<<"Podaj nowy adres: ";
+                getline(cin, osoby[i].adres);
+                break;
+            }
+            zapisDoPliku(osoby, liczbaOsob);
+        }
+    }
+    if(znalezionoOsobe==false)
+        cout<<"Nie ma w ksiazce osoby o podanym ID.";
 }
 
 int main() {
     char wybor;
     int liczbaOsob=0;
-    Osoba *osoby = new Osoba[50];
+    vector<Osoba> osoby;
 
     odczytZPliku(osoby, liczbaOsob);
 
@@ -126,6 +243,8 @@ int main() {
         cout<<"2. Wyszukaj osobe po imieniu."<<endl;
         cout<<"3. Wyszukaj osobe po nazwisku."<<endl;
         cout<<"4. Wyswietl wszystkie osoby z ksiazki."<<endl;
+        cout<<"5. Usun osobe."<<endl;
+        cout<<"6. Edytuj osobe."<<endl;
         cout<<"0. Wyjdz z programu."<<endl;
 
         wybor=getch();
@@ -147,6 +266,14 @@ int main() {
             wyswietlListeOsob(osoby, liczbaOsob);
             getch();
             break;
+        case '5':
+            usunOsobe(osoby, liczbaOsob);
+            getch();
+            break;
+        case '6':
+            edytujOsobe(osoby, liczbaOsob);
+            getch();
+            break;
         case '0':
             cout<<"Zegnaj :(";
             Sleep(1000);
@@ -159,6 +286,6 @@ int main() {
         }
         system("cls");
     }
-
-    delete[] osoby;
+    return 0;
 }
+
